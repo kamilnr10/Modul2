f1e355655d924608645ebbc2647d08dc4c37b3a0
Kamilnr10 <kamil.nr10@gmail.com>
2020-03-16 23:01:52 +0100

    Task 4. Working on class Rent

--- task4.js
+++ task4.js
@@ -5,32 +5,29 @@
 // Ma miec: Tytuł, Autora, id(kod)
 // Ma umożliwiać: Zmianę id
 
-// Obiekt charakteryzujący bibliotekę:
-// Ma miec: Listę książek(obiektów) z różnymi autorami, tytułami (około 8-15).
-// Ma umożliwiać: dodawanie książek do listy, usuwanie książek z listy
-
-// Obiekt charakteryzujący wypożyczenie:
-// Ma mieć: datę wypożyczenia, datę zwrotu( +7d od wypożyczenia), id wypożyczonej
-// pozycji, jej tytuł. kara
-// Ma umożliwiać: wypożyczenie ksiązki (jesli książki nie ma w liście - jest niedostepna/
-// wypożyczona ma zwracać informację) jesli jest dostępna usuwać książkę z listy
-// dostępnych, ma umożliwiać zwrot - jeśli odbędzie się terminowo kara jest 0 - jesli nie -
-// każdy dzień zwłoki to naliczenie jakiejś kary. Przy zwrocie książka wraca na liste.
-
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
     this.id = uuidv4().substr(3, 3);
+
+    if (title === "" || author === "") {
+      throw Error(`Title and author are required (You can't leave this blank)`);
+    }
   }
+
   read() {
     return `Title: ${this.title} \n Author: ${this.author} \n ID: ${this.id}`;
   }
-  update(id) {
-    if (id.length === 3) {
-      this.id = id;
-      return `ID has been updated`;
-    } else return `ID must have 3 signs`;
+  update(key, value) {
+    if (Object.keys(this).includes(key)) {
+      if (key === id) {
+        return `Cannot change ID of book`;
+      } else {
+        this[key] = value;
+        return `Book ${key} has been updated`;
+      }
+    }
   }
 }
 
@@ -45,6 +42,10 @@ const book8 = new Book("Mechaniczny Anioł", "Cassandra Clare");
 const book9 = new Book("Zieleń Szmaragdu", "Kerstin Gier");
 const book10 = new Book("Cień wiatru", "Carlos Ruiz Zafon");
 
+// Obiekt charakteryzujący bibliotekę:
+// Ma miec: Listę książek(obiektów) z różnymi autorami, tytułami (około 8-15).
+// Ma umożliwiać: dodawanie książek do listy, usuwanie książek z listy
+
 class Library {
   constructor(name, ...listOfBooks) {
     this.name = name;
@@ -52,20 +53,29 @@ class Library {
     this.listOfRentedBooks = [];
   }
 
-  addBookToList(book) {
+  addBookToLibrary(book) {
     this.listOfBooks.push(book);
     return `Book has been added to Library`;
   }
 
-  deleteBookFromList(book) {
+  deleteBookFromLibrary(book) {
     const findBook = this.listOfBooks.filter(item => item === book)[0];
     this.listOfBooks = this.listOfBooks.filter(item => item !== book);
     return `Book: ${findBook.title} has been deleted from Library`;
   }
 
-  showAllBooks() {}
-  showAvailableBooks() {}
-  showRentedBooks() {}
+  showAllBooks() {
+    const allBooksInLibrary = this.listOfBooks.concat(this.listOfRentedBooks);
+    return allBooksInLibrary;
+  }
+
+  showAvailableBooks() {
+    return this.listOfBooks;
+  }
+
+  showRentedBooks() {
+    return this.listOfRentedBooks;
+  }
 }
 
 const library = new Library(
@@ -82,18 +92,27 @@ const library = new Library(
   book10
 );
 
+// Obiekt charakteryzujący wypożyczenie:
+// Ma mieć: datę wypożyczenia, datę zwrotu( +7d od wypożyczenia), id wypożyczonej
+// pozycji, jej tytuł. kara
+// Ma umożliwiać: wypożyczenie ksiązki (jesli książki nie ma w liście - jest niedostepna/
+// wypożyczona ma zwracać informację) jesli jest dostępna usuwać książkę z listy
+// dostępnych, ma umożliwiać zwrot - jeśli odbędzie się terminowo kara jest 0 - jesli nie -
+// każdy dzień zwłoki to naliczenie jakiejś kary. Przy zwrocie książka wraca na liste.
+
 class Rent {
-  constructor(name, dateOfRent, dateOfReturn, title, author, penalty) {
-    this.name = name;
-    this.dateOfRent = dateOfRent;
-    this.dateOfReturn = dateOfReturn;
-    this.id = uuidv4();
-    this.title = title;
-    this.author = author;
-    this.penalty = penalty;
+  constructor(book) {
+    this.name = book;
+    this.dateOfRent = moment().format("l");
+    this.dateOfReturn = moment()
+      .add(7, "days")
+      .calendar();
+    this.penalty = 0;
   }
 
-  rentBook(book) {}
+  rentBook(book) {
+    library.listOfRentedBooks.push(book);
+  }
   returnBook(book) {}
 
   payPenalty(penalty) {}
